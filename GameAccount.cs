be007523071c24cc79c3b6881bee847637dcc196
Andrii Cherevatyi <andreycherevat@gmail.com>
2022-10-15 16:33:14 +0200

    commiting previous changes

--- GameAccount.cs
+++ GameAccount.cs
@@ -6,7 +6,35 @@ namespace OOP_Lab_1
 {
     class GameAccount
     {
-        private string UserName { get; }
+        private const int DFLT_RATING = 100;
 
+        private static int GamesPlayedByAllPlayers { get; } = 0;
+
+        private string UserName { get; }
+        private int CurrentRating { get; }
+        private int GamesCount { get; }
+
+        public GameAccount(string UserName)
+        {
+            if (UserName.Length < 1) this.UserName = "guest";
+            else this.UserName = UserName;
+            CurrentRating = DFLT_RATING;
+            GamesCount = 0;
+        }
+
+        public void WinGame(/*Match game*/)
+        {
+            return;
+        }
+
+        public void LoseGame(/*Match game*/)
+        {
+            return;
+        }
+
+        public void getStats()
+        {
+            return;
+        }
     }
 }
