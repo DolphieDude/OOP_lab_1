d8095d9b28882f73623e764a700128ba77bfef21
Andrii Cherevatyi <andreycherevat@gmail.com>
2022-10-16 02:00:00 +0200

    commiting changes

--- GameAccount.cs
+++ GameAccount.cs
@@ -14,22 +14,33 @@ namespace OOP_Lab_1
         public int CurrentRating { get; }
         public int GamesCount { get; }
 
+        private List<Match> Matches;
+
         public GameAccount(string UserName)
         {
             if (UserName.Length < 1) this.UserName = "guest";
             else this.UserName = UserName;
             CurrentRating = DFLT_RATING;
             GamesCount = 0;
+            Matches = new List<Match>();
         }
 
-        public void WinGame(/*Match game*/)
-        {
+        /*
+        public void WinGame(Match game)
+        {   
+
             return;
         }
 
-        public void LoseGame(/*Match game*/)
+        public void LoseGame(Match game)
         {
             return;
+        }
+        */
+        //I've decided not to implement these methods because in my opinion results should be stored in Matches itselves
+        public void Play(Match match)
+        {
+            Matches.Add(match);
         }
 
         public void getStats()
--- Match.cs
+++ Match.cs
@@ -8,14 +8,16 @@ namespace OOP_Lab_1
     {
         private const int DFLT_RATING_CHANGE = 20;
 
-        private GameAccount player1 { get; }
-        private GameAccount player2 { get; }
+        private GameAccount Player1 { get; }
+        private GameAccount Player2 { get; }
         private int RatingChange1 { get; }
         private int RatingChange2 { get; }
-        private GameAccount winner { get; set; }
+        public GameAccount Winner { get; }
 
         public Match(GameAccount player1, GameAccount player2)
         {
+            //I am not sure it's okay to compare objects like this. Should test it out
+            if (player1 == player2) throw new ArgumentException("You can't play using the same player");
             if (player1.CurrentRating >= player2.CurrentRating)
             {
                 RatingChange1 = DFLT_RATING_CHANGE;
@@ -25,6 +27,25 @@ namespace OOP_Lab_1
             {
                 RatingChange2 = DFLT_RATING_CHANGE;
                 RatingChange1 = DFLT_RATING_CHANGE + (player2.CurrentRating - player1.CurrentRating) / 2;
+            }
+            
+            this.Player1 = player1;
+            this.Player2 = player2;
+
+            int coinFlip = new Random().Next(1, 100);
+            if (coinFlip <= 50)
+            {
+                Console.WriteLine(player1.UserName + " has won!");
+                Winner = player1;
+                player1.Play(this);
+                player2.Play(this);
+            }
+            else
+            {
+                Console.WriteLine(player2.UserName + " has won!");
+                Winner = player2;
+                player1.Play(this);
+                player2.Play(this);
             }
         }
     }
