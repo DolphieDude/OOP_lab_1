a3f7eadec1f8e0f8896285f5946587ee96858ec3
Andrii Cherevatyi <andreycherevat@gmail.com>
2022-10-16 02:26:45 +0200

    updated Match.cs and finished GameAccount.cs

--- GameAccount.cs
+++ GameAccount.cs
@@ -8,11 +8,9 @@ namespace OOP_Lab_1
     {
         private const int DFLT_RATING = 100;
 
-        public static int GamesPlayedByAllPlayers { get; } = 0;
-
         public string UserName { get; }
-        public int CurrentRating { get; }
-        public int GamesCount { get; }
+        public int CurrentRating { get; set}
+        public int GamesCount { get; set; }
 
         private List<Match> Matches;
 
@@ -38,13 +36,30 @@ namespace OOP_Lab_1
         }
         */
         //I've decided not to implement these methods because in my opinion results should be stored in Matches itselves
-        public void Play(Match match)
+        public void Play(Match match, int ratingChange)
         {
             Matches.Add(match);
+            GamesCount++;
+            CurrentRating += ratingChange;
+            if (CurrentRating < 1) CurrentRating = 1;
         }
 
         public void getStats()
         {
+            Console.WriteLine(UserName + " played " + GamesCount + "games:");
+            string currLine;
+            foreach (Match match in Matches)
+            {
+                currLine = "Match id" + match.MatchIndex + ": ";
+                
+                if (this == match.Winner) currLine += "Win; ";
+                else currLine += "Lose; ";
+
+                if (this == match.Player1) currLine += match.RatingChange1 + " rating points; Opponent " + match.Player2.UserName + ";";
+                else currLine += match.RatingChange2 + " rating points; Opponent " + match.Player1.UserName + ";";
+
+                Console.WriteLine(currLine);
+            }
             return;
         }
     }
--- Match.cs
+++ Match.cs
@@ -8,16 +8,23 @@ namespace OOP_Lab_1
     {
         private const int DFLT_RATING_CHANGE = 20;
 
-        private GameAccount Player1 { get; }
-        private GameAccount Player2 { get; }
-        private int RatingChange1 { get; }
-        private int RatingChange2 { get; }
+        private static int OverallGamesPlayed { get; set; } = 0;
+
+        public GameAccount Player1 { get; }
+        public GameAccount Player2 { get; }
+        public int RatingChange1 { get; }
+        public int RatingChange2 { get; }
         public GameAccount Winner { get; }
+        public int MatchIndex { get; }
 
         public Match(GameAccount player1, GameAccount player2)
         {
             //I am not sure it's okay to compare objects like this. Should test it out
             if (player1 == player2) throw new ArgumentException("You can't play using the same player");
+
+            OverallGamesPlayed++;
+            this.MatchIndex = OverallGamesPlayed;
+
             if (player1.CurrentRating >= player2.CurrentRating)
             {
                 RatingChange1 = DFLT_RATING_CHANGE;
@@ -37,15 +44,17 @@ namespace OOP_Lab_1
             {
                 Console.WriteLine(player1.UserName + " has won!");
                 Winner = player1;
-                player1.Play(this);
-                player2.Play(this);
+                RatingChange2 = -RatingChange2;
+                player1.Play(this, RatingChange1);
+                player2.Play(this, RatingChange2);
             }
             else
             {
                 Console.WriteLine(player2.UserName + " has won!");
                 Winner = player2;
-                player1.Play(this);
-                player2.Play(this);
+                RatingChange1 = -RatingChange1;
+                player1.Play(this, RatingChange1);
+                player2.Play(this, RatingChange2);
             }
         }
     }
